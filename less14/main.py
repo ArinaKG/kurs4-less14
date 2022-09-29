96c1b0bbbed489e6693310c1cd41dbc0de8c8198
Арина <a.kornishina@mail.ru>
2022-09-29 21:53:26 +0300

    окончательный правильный рабочий вариант

--- less14/main.py
+++ less14/main.py
@@ -1,7 +1,6 @@
 import json
 import sqlite3
 import flask
-import tmp as tmp
 
 app = flask.Flask(__name__)
 
@@ -15,12 +14,11 @@ def get_value_from_db(sql):
         return result
 
 
-@app.get("/movie/<title>/")
 def get_value_by_title(title):
-    sql = (f'select title, country, release_year, listed_in as genre, description from netflix\'\n'
-           f'    where title = \'{title}\'\n'
-           f'    order by release_year desc\n'
-           f'    limit 1\n'
+    sql = (f'select title, country, release_year, listed_in as genre, description from netflix'
+           f'    where title = {title} '
+           f'    order by release_year desc'
+           f'    limit 1'
            f'    ')
 
     result = get_value_from_db(sql)
@@ -67,15 +65,14 @@ def det_by_date(year1, year2):
 
 @app.get("/rating/<rating>/")
 def get_by_rating(rating):
-    my_dict= {
-            "children": ("G", "G"),
-            "family": ("G", "PG", "PG-13"),
-            "adult": ("R", "NC-17")}
-
+    my_dict = {
+        "children": ("G", "G"),
+        "family": ("G", "PG", "PG-13"),
+        "adult": ("R", "NC-17")}
 
     sql = f'''
             select title, rating, description from netflix
-            where rating in {my_dict.get(rating), ("G", "NC-17")}'''
+            where rating in {my_dict.get(rating, ("G", "NC-17"))} '''
     result = get_value_from_db(sql)
 
     tmp = []
@@ -92,11 +89,11 @@ def get_by_rating(rating):
     )
 
 
-@app.get("/genre/<genre>")
+@app.get("/genre/<genre>/")
 def get_by_genre(genre):
     sql = f'''
             select title, description from netflix
-            where listed_in like 'genre' '''
+            where listed_in like '%{genre}%' '''
 
     result = get_value_from_db(sql)
 
@@ -110,7 +107,7 @@ def get_by_genre(genre):
                             indent=4
                             ),
         status=200,
-        mimetypes='application/json'
+        mimetype="application/json"
 
     )
 
@@ -130,9 +127,9 @@ where "cast" like '%{name1}%' and "cast" like '%{name2}%' '''
         names_dict[name.strip()] = names_dict.get(name.strip, 0) + 1
 
     for key, value in names_dict.items():
-        if value >2:
+        if value > 2:
             tmp.append(key)
-            
+
     return tmp
 
 
@@ -151,10 +148,166 @@ def get_ditales(typ, year, genre):
         tmp.append(dict(item))
 
     return json.dumps(tmp,
+                      ensure_ascii=False,
+                      indent=4
+                      )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+import json
+import sqlite3
+import flask
+
+app = flask.Flask(__name__)
+
+
+def get_value_from_db(sql):
+    with sqlite3.connect("netflix.db") as connection:
+        connection.row_factory = sqlite3.Row
+
+        result = connection.execute(sql).fetchall()
+
+        return result
+
+
+def get_value_by_title(title):
+    sql = (f'select title, country, release_year, listed_in as genre, description from netflix'
+           f'    where title = {title} '
+           f'    order by release_year desc'
+           f'    limit 1'
+           f'    ')
+
+    result = get_value_from_db(sql)
+
+    for item in result:
+        return dict(item)
+
+
+@app.get("/movie/<title>/")
+def view_title(title):
+    result = get_value_by_title(title)
+    return app.response_class(
+        response=json.dumps(result,
                             ensure_ascii=False,
                             indent=4
-                            )
+                            ),
+        status=200,
+        mimetype="application/json"
 
+    )
+
+
+@app.get("/movie/<int:year1>/to/<int:year2>/")
+def det_by_date(year1, year2):
+    sql = f'''
+            select title, release_year from netflix
+            where release_year between {year1} and {year2}
+            limit 100'''
+    result = get_value_from_db(sql)
+
+    tmp = []
+    for item in result:
+        tmp.append(dict(item))
+    return app.response_class(
+        response=json.dumps(tmp,
+                            ensure_ascii=False,
+                            indent=4
+                            ),
+        status=200,
+        mimetype="application/json"
+
+    )
+
+
+@app.get("/rating/<rating>/")
+def get_by_rating(rating):
+    my_dict = {
+        "children": ("G", "G"),
+        "family": ("G", "PG", "PG-13"),
+        "adult": ("R", "NC-17")}
+
+    sql = f'''
+            select title, rating, description from netflix
+            where rating in {my_dict.get(rating, ("G", "NC-17"))} '''
+    result = get_value_from_db(sql)
+
+    tmp = []
+    for item in result:
+        tmp.append(dict(item))
+    return app.response_class(
+        response=json.dumps(tmp,
+                            ensure_ascii=False,
+                            indent=4
+                            ),
+        status=200,
+        mimetype="application/json"
+
+    )
+
+
+@app.get("/genre/<genre>/")
+def get_by_genre(genre):
+    sql = f'''
+            select title, description from netflix
+            where listed_in like '%{genre}%' '''
+
+    result = get_value_from_db(sql)
+
+    tmp = []
+    for item in result:
+        tmp.append(dict(item))
+
+    return app.response_class(
+        response=json.dumps(tmp,
+                            ensure_ascii=False,
+                            indent=4
+                            ),
+        status=200,
+        mimetype="application/json"
+
+    )
+
+
+def get_by_names(name1, name2, tmp=None):
+    sql = f'''
+    select * from netflix
+where "cast" like '%{name1}%' and "cast" like '%{name2}%' '''
+
+    result = get_value_from_db(sql)
+
+    names_dict = {}
+    for item in result:
+        names = set(dict(item).get("cast").split(", ")) - set(name1, name2)
+
+    for name in names:
+        names_dict[name.strip()] = names_dict.get(name.strip, 0) + 1
+
+    for key, value in names_dict.items():
+        if value > 2:
+            tmp.append(key)
+
+    return tmp
+
+
+def get_ditales(typ, year, genre):
+    sql = f'''
+        select * from netflix
+        where type = '{typ}' and
+        release_year = '{year}' and
+        listed_in like '%{genre}%'
+'''
+
+    result = get_value_from_db(sql)
+
+    tmp = []
+    for item in result:
+        tmp.append(dict(item))
+
+    return json.dumps(tmp,
+                      ensure_ascii=False,
+                      indent=4
+                      )
 
 
 if __name__ == '__main__':
